5e016f7e1e243cd1361d57fdb6436f2f911aef35
Lucas Zelaya <zelaya4102@gmail.com>
2025-04-19 00:11:15 -0300

    Se creo la funcion buscarPrecioPizza y se agrego el ciclo while

--- EjercicioPizzeria/Program.cs
+++ EjercicioPizzeria/Program.cs
@@ -8,13 +8,40 @@ namespace EjercicioPizzeria
 {
     internal class Program
     {
+        int buscarPrecioPizza(int pizzaSeleccionada, int[] pizzas)
+        {
+
+            int pizzaPrecio;
+
+
+            if (pizzaSeleccionada >= 0 && pizzaSeleccionada < pizzas.Length)
+            {
+                pizzaPrecio = pizzas[pizzaSeleccionada];
+                return pizzaPrecio;
+            }
+            else
+            {
+                Console.WriteLine("La pizza seleccionada no existe");
+            }
+
+            return -1;
+
+        }
+
+        int precioPedido(int pizzasPedidas, int precioPizza)
+        {
+
+
+        }
+
+
         static void Main(string[] args)
         {
             int[] preciosPizzas = { 12, 15, 20, 25, 32 };
-            int[] moto = { 1, 2, 3, 4, 5 };
+            string[] motos = { "Fran Zanella", "Axel Gilera", "Luciano Harley", "Lucas Bmw", "Liu Suzuki" };
+            List<int> pedidos;
 
-
-            int iniciarSistema,codPizza, cantidad, nroMoto, monto, montoTotal;
+            int iniciarSistema,codPizza, cantidad, nroMoto, monto, montoTotal, distancia,nroPedido;
             codPizza = 0;
             cantidad = 0;
             nroMoto = 0;
@@ -22,38 +49,64 @@ namespace EjercicioPizzeria
             montoTotal = 0;
 
 
-            int buscarPizza(int pizzaSeleccionada, int[] Pizzas  ){
+            
 
-                for (int i = 0; int i < Pizzas.Length; int i++)
-                {
-                    
-                }
-
-
-            }
 
             Console.WriteLine("🍕 Pedidos Pizerria 🍕");
 
-            Console.WriteLine("Si desea ingresar un pedido ingrese 1 sino 0 para salir del sistemas");
-            iniciarSistema = int.Parse(Console.ReadLine());
-            if (iniciarSistema == 1)
+            Console.WriteLine("Si desea ingresar un pedido ingrese 1 sino 0 para salir del sistema");
+            while (true)
             {
-                Console.WriteLine("----  Bievenido al sistema de Pedidos 🍕 -----");
-                Console.WriteLine("Por favor ingrese el numero de Pizza que desea elegir:");
-                Console.WriteLine("");
-                Console.WriteLine("--  0) Muzzarella Grande");
-                Console.WriteLine("--  1) Napolitana Grande");
-                Console.WriteLine("--  2) Anchoas Grande");
-                Console.WriteLine("--  3) Jamon y Morrones Grande");
-                Console.WriteLine("--  4) Fugazzetta Grande");
-                Console.WriteLine("");
-                codPizza = int.Parse(Console.ReadLine());
+                Console.Write("Numero:");
+                iniciarSistema = int.Parse(Console.ReadLine());
+                    if (iniciarSistema == 1)
+                    {
+                        Console.WriteLine("----  Bievenido al sistema de Pedidos 🍕 -----");
+                        Console.WriteLine("");
 
+                        Console.WriteLine("Ingrese el numero de Pedido 📃📃 :");
+                        Console.Write("Numero:");
+                        nroPedido = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
 
-            }
-            else
-            {
-                Console.WriteLine("Saliendo del sistema...");
+                        Console.WriteLine("Ingrese el numero de Pizza a selecionar:");
+                        Console.WriteLine("");
+                        Console.WriteLine("--  0) Muzzarella Grande");
+                        Console.WriteLine("--  1) Napolitana Grande");
+                        Console.WriteLine("--  2) Anchoas Grande");
+                        Console.WriteLine("--  3) Jamon y Morrones Grande");
+                        Console.WriteLine("--  4) Fugazzetta Grande");
+                        Console.WriteLine("");
+                        Console.Write("Numero:");
+                        codPizza = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Ingrese la cantidad de Pizzas:");
+                        Console.Write("Cantidad:");
+                        cantidad = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Ingrese el numero de moto que repartira el pedido:");
+                        Console.Write("Numero:");
+                        nroMoto = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Ingrese los km que hara el delivery: ");
+                        Console.Write("Kilometros:");
+                        distancia = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
+
+                    }
+                    else if(iniciarSistema <=0 || iniciarSistema>1 )
+                    {
+                        Console.WriteLine("Error solo se puede ingresar 1 o 0 🙃🙃 ...");
+                }
+                else
+                {
+                    Console.WriteLine("Saliendo del sistema 👋👋👋 ....");
+                    break;
+                }
+
             }
         }
     }
